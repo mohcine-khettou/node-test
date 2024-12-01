f735885a95c9e4c9c1768a20ac9bc85362364f10
mohcine-khettou <malakchams006@gmail.com>
2024-12-01 22:39:06 +0100

    adding dotenv

--- server.js
+++ server.js
@@ -4,7 +4,4 @@ require("dotenv").config();
 const insuranceRouter = require("./insurance-route");
 app.use("/", insuranceRouter);
 
-const port = process.env.port || 5000;
-app.listen(port, () => {
-  console.log("app is listening on port 5000");
-});
+module.exports = app;
